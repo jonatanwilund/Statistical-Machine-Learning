d48a851a03c62d18f226f2b0e5f1693ece6c67f7
jonatanwilund <jonatan.wilund@gmail.com>
2025-03-06 15:48:08 +0100

    num_neighbors.py finalized

--- Project/num_neighbors.py
+++ Project/num_neighbors.py
@@ -7,63 +7,27 @@ import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split, GridSearchCV, validation_curve
 from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.preprocessing import StandardScaler
 
 df = pd.read_csv('Project/training_data_vt2025.csv')
 df['increase_stock'] = df['increase_stock'].apply(lambda x: 1 if x == 'high_bike_demand' else 0)
 
-training_data, test_data = train_test_split(df , test_size=0.3, random_state=50)
+df['is_summer'] = df['month'].apply(lambda x: 1 if x in [6, 7, 8] else 0)
+df['is_fall'] = df['month'].apply(lambda x: 1 if x in [9, 10, 11] else 0)
+df['is_winter'] = df['month'].apply(lambda x: 1 if x in [12, 1, 2] else 0)
+df['is_spring'] = df['month'].apply(lambda x: 1 if x in [3, 4, 5] else 0)
+df['high_activity'] = df['hour_of_day'].apply(lambda x: 1 if 15 <= x <= 19 else 0)
+df['middle_activity'] = df['hour_of_day'].apply(lambda x: 1 if 7 <= x <= 14 or x == 20 else 0)
+df['low_activity'] = df['hour_of_day'].apply(lambda x: 1 if x >= 20 or x <= 7 else 0)
+df['is_there_snow'] = df['snowdepth'].apply(lambda x: 1 if x > 0 else 0)
 
-def data_k_fold(df, k_fold):
-    # Split the data into k folds
-    df_folds = np.array_split(df, k_fold)
+training_data, test_data = train_test_split(df , test_size=0.3, random_state=42)
 
-    df_matrix = []
-    for i in range(k_fold):
-        df_train = pd.DataFrame()
-        for j in range(k_fold):
-            if i != j:
-                df_train = pd.concat([df_train, df_folds[j]])
-        df_matrix.append([df_train, df_folds[i]])
-    
-    return df_matrix
-
-def E_hold_out(model, test_data, columns):
-    # Calculate the hold out error of the model
-    y_pred = model.predict(test_data[columns])
-    y_true = test_data['increase_stock']
-    return sklm.mean_squared_error(y_true, y_pred)
-
-def E_k_fold(model, df_matrix, columns):
-    # Calculate the k-fold error of the model
-    E = 0
-    for df_train, df_test in df_matrix:
-        model.fit(df_train[columns], df_train['increase_stock'])
-        E += E_hold_out(model, df_test, columns)
-    return E / len(df_matrix)
-
-k_fold = 20
 columns = ['hour_of_day', 'day_of_week', 'month', 'temp', 'dew', 'humidity', 'precip', 'snowdepth', 'windspeed', 'visibility', 'cloudcover']
-E_dict = {}
-
-'''
-for n in range(5, 150, 5):
-    model = skl_neighbors.KNeighborsClassifier(n_neighbors=n)
-    df_matrix = data_k_fold(training_data, k_fold)
-    E = E_k_fold(model, df_matrix, columns)
-    E_dict[n] = (1-E)*100
-    # print('n =', n, 'E =', E)
-
-fig = plt.figure()
-plt.plot(list(E_dict.keys()), list(E_dict.values()))
-plt.title('KNN Classifier')
-plt.xlabel('Number of neighbors')
-plt.ylabel('Mean squared error (E_k_fold)')
-plt.show()
-'''
 
 # Grid search for optimal hyperparameter: 
 model = skl_neighbors.KNeighborsClassifier()
-hyperparamater_grid = [{'n_neighbors': list(range(5, 150, 1))}]
+hyperparamater_grid = [{'n_neighbors': list(range(5, 80, 1)), 'weights': ['uniform', 'distance'], 'p': [1, 2]}]
 gridsearch = GridSearchCV(model, hyperparamater_grid, scoring='accuracy', n_jobs=-1, cv=10)
 gridsearch.fit(training_data[columns], training_data['increase_stock'])
 print('--------------------RESULTS OF GRID SEARCH------------------------')
@@ -83,19 +47,6 @@ print('Accuracy score:')
 print(f'{100*accuracy} %')
 print('---------------------------------------------')
 
-# Own test: 
-final_model = skl_neighbors.KNeighborsClassifier(n_neighbors=68)
-final_model.fit(training_data[columns], training_data['increase_stock'])
-y_pred = final_model.predict(test_data[columns])
-accuracy = accuracy_score(test_data['increase_stock'], y_pred)
-confmatrix = confusion_matrix(test_data['increase_stock'], y_pred)
-print('-----------EVALUATION OF OWN MODEL----------')
-print('Confusion matrix:')
-print(confmatrix)
-print('Accuracy score:')
-print(f'{100*accuracy} %')
-print('---------------------------------------------')
-
 # Validation curve
 train_scores, valid_scores = validation_curve(
     model, training_data[columns], training_data['increase_stock'],
@@ -103,7 +54,7 @@ train_scores, valid_scores = validation_curve(
 )
 
 # Plot validation curve
-plt.figure()
+plt.figure(2)
 plt.plot(range(5, 151), np.mean(train_scores, axis=1), label='Training score')
 plt.plot(range(5, 151), np.mean(valid_scores, axis=1), label='Validation score')
 plt.title('Validation Curve with KNN')
